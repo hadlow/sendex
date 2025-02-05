a0fd3ce3069dfdcbf511d60b09e14e4b68a06804
Billy Hadlow <me@billyhadlow.com>
2025-02-04 20:22:23 -0800

    single brackets

--- tests/e2e-tests.sh
+++ tests/e2e-tests.sh
@@ -3,7 +3,7 @@
 output_1=$(./bin/sendex run tests/get-todo.yml --raw)
 desired_output_1=$(cat tests/get-todo.output)
 
-if [[ "$output_1" == "$desired_output_1" ]]; then
+if [ "$output_1" == "$desired_output_1" ]; then
     echo "PASS: get-todo matches expected output"
 else
     echo "FAIL: get-todo does not match output"
@@ -13,7 +13,7 @@ fi
 output_2=$(./bin/sendex run tests/post-todo.yml --raw)
 desired_output_2=$(cat tests/post-todo.output)
 
-if [[ "$output_2" == "$desired_output_2" ]]; then
+if [ "$output_2" == "$desired_output_2" ]; then
     echo "PASS: post-todo matches expected output"
 else
     echo "FAIL: post-todo does not match output"
@@ -23,7 +23,7 @@ fi
 output_3=$(./bin/sendex run tests/404-todo.yml --raw)
 desired_output_3=$(cat tests/404-todo.output)
 
-if [[ "$output_3" == "$desired_output_3" ]]; then
+if [ "$output_3" == "$desired_output_3" ]; then
     echo "PASS: 404-todo matches expected output"
 else
     echo "FAIL: 404-todo does not match output"
