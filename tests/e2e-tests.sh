e79faf99a001cf4ce294863de7dc0ebd585748a6
Billy Hadlow <me@billyhadlow.com>
2025-02-08 22:11:45 -0800

    tests: add command tests

--- tests/e2e-tests.sh
+++ tests/e2e-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Standard GET
 output_1=$(./bin/sendex run tests/get-todo.yml --raw)
 desired_output_1=$(cat tests/get-todo.output)
 
@@ -10,6 +11,7 @@ else
     exit 1
 fi
 
+# Standard POST
 output_2=$(./bin/sendex run tests/post-todo.yml --raw)
 desired_output_2=$(cat tests/post-todo.output)
 
@@ -20,12 +22,46 @@ else
     exit 1
 fi
 
-output_3=$(./bin/sendex run tests/404-todo.yml --raw)
-desired_output_3=$(cat tests/404-todo.output)
+# GET but only headers
+output_3=$(./bin/sendex run tests/get-todo.yml -e --raw)
+desired_output_3=$(cat tests/get-todo-headers.output)
 
 if [ "$output_3" = "$desired_output_3" ]; then
-    echo "PASS: 404-todo matches expected output"
+    echo "PASS: get-todo-headers matches expected output"
 else
-    echo "FAIL: 404-todo does not match output"
+    echo "FAIL: get-todo-headers does not match output"
+    exit 1
+fi
+
+# GET but only body
+output_4=$(./bin/sendex run tests/get-todo.yml -b --raw)
+desired_output_4=$(cat tests/get-todo-body.output)
+
+if [ "$output_4" = "$desired_output_4" ]; then
+    echo "PASS: get-todo-body matches expected output"
+else
+    echo "FAIL: get-todo-body does not match output"
+    exit 1
+fi
+
+# GET but only status code
+output_5=$(./bin/sendex run tests/get-todo.yml -s --raw)
+desired_output_5=$(cat tests/get-todo-status.output)
+
+if [ "$output_5" = "$desired_output_5" ]; then
+    echo "PASS: get-todo-status matches expected output"
+else
+    echo "FAIL: get-todo-status does not match output"
+    exit 1
+fi
+
+# GET with formatting
+output_6=$(./bin/sendex run tests/get-todo.yml)
+desired_output_6=$(cat tests/get-todo-formatted.output)
+
+if [ "$output_6" = "$desired_output_6" ]; then
+    echo "PASS: get-todo-formatted matches expected output"
+else
+    echo "FAIL: get-todo-formatted does not match output"
     exit 1
 fi
